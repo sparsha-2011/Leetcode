d1c85511ff785bd7f262fbee455a6822a3358568
Sparsha Srinath <70736797+Sparsha1303@users.noreply.github.com>
2025-05-25 11:55:27 -0700

    Create permutations.py

---
+++ Backtracking/permutations.py
@@ -0,0 +1,45 @@
+# Date: 2025-04-15
+# Author: Sparsha Srinath
+# Leetcode (Permutations): https://leetcode.com/problems/permutations/
+# Tags: Backtracking, DFS, Recursion, Arrays
+
+from typing import List
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        """
+        Generates all possible permutations of a list of distinct integers.
+
+        Args:
+            nums (List[int]): The list of distinct integers.
+
+        Returns:
+            List[List[int]]: A list containing all possible permutations.
+        """
+        res = []       # Final result list to store all permutations
+        cur = []       # Temporary list to build current permutation
+        used = set()   # Set to track elements already used in current permutation
+
+        def dfs(cur: List[int]) -> None:
+            """
+            Recursive DFS helper to build permutations.
+
+            Args:
+                cur: Current permutation being built.
+            """
+            # Base case: if the current permutation is complete
+            if len(cur) == len(nums):
+                res.append(cur.copy())
+                return
+
+            # Try each number that hasn't been used yet
+            for i in range(len(nums)):
+                if nums[i] not in used:
+                    used.add(nums[i])       # Mark the number as used
+                    cur.append(nums[i])     # Add number to current permutation
+                    dfs(cur)                # Recurse
+                    used.remove(nums[i])    # Backtrack: unmark the number
+                    cur.pop()               # Remove number from current path
+
+        dfs(cur)
+        return res
