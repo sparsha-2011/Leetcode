516ed7b8af9509b96e0e871678d46aee42a569a1
Sparsha Srinath <70736797+Sparsha1303@users.noreply.github.com>
2025-06-04 18:12:23 -0700

    Update permutations.py

--- Backtracking/permutations.py
+++ Backtracking/permutations.py
@@ -43,3 +43,34 @@ class Solution:
 
         dfs(cur)
         return res
+
+##Approach 2
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+
+        used = [False]*len(nums)
+        res = []
+        cur = []
+
+        def dfs(cur):
+
+            if len(cur) == len(nums):
+                res.append(cur.copy())
+            
+            for i in range(len(nums)):
+                if used[i]:
+                    continue
+
+                #if duplicates are present uncomment below code
+                # if i >0 and nums[i]==nums[i-1] and not used[i-1]:
+                #     continue
+                
+                used[i] = True
+                cur.append(nums[i])
+                dfs(cur)
+                used[i] = False
+                cur.pop()
+                
+        dfs(cur)
+        return res
+        
