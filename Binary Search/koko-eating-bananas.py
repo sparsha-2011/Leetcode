6dcfd6d68d194b6da5687e0f33c5bb09be2f3953
Sparsha Srinath <70736797+Sparsha1303@users.noreply.github.com>
2025-08-13 18:17:15 -0700

    Create koko-eating-bananas.py

---
+++ Binary Search/koko-eating-bananas.py
@@ -0,0 +1,34 @@
+# Author: Sparsha Srinath
+# Date: 2025-08-13
+# Problem: Koko Eating Bananas
+# Link: https://leetcode.com/problems/koko-eating-bananas/
+# Tags: Binary Search, Math
+# Time Complexity: O(n log m)  # n = number of piles, m = max pile size
+# Space Complexity: O(1)
+
+from typing import List
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        
+        start = 1
+        res = -1
+        end = max(piles)
+
+        def isValid(arr, h, mid):
+            if not mid:
+                return False
+            time = 0
+            for i in arr:
+                time += (i + mid - 1) // mid
+            return time <= h
+
+        while start <= end:
+            mid = (start + end) // 2
+            if isValid(piles, h, mid):
+                res = mid
+                end = mid - 1
+            else:
+                start = mid + 1
+        
+        return res
