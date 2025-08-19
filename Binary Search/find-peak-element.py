d6e677f33b41c37b8dd2a1ceb2c5dba5855d0a14
Sparsha Srinath <70736797+Sparsha1303@users.noreply.github.com>
2025-08-19 13:46:11 -0700

    Update find-peak-element.py

--- Binary Search/find-peak-element.py
+++ Binary Search/find-peak-element.py
@@ -6,9 +6,25 @@
 # Time Complexity: O(log n)
 # Space Complexity: O(1)
 
-from typing import List
 
-class Solution:
+class Solution1:
+    def findPeakElement(self, nums: List[int]) -> int:
+        N = len(nums)
+
+        start = 0
+        end = N-1
+        res = end
+        while start<=end:
+            mid = (start+end)//2
+            if mid+1<N and nums[mid]>nums[mid+1]:
+                res = mid
+                end = mid -1
+            else:
+                start = mid +1
+                
+        return res
+
+class Solution2:
     def findPeakElement(self, nums: List[int]) -> int:
         N = len(nums)
         start, end = 0, N - 1
