d33986620158f9a6de062d3f69c42527b27c31f1
Sparsha Srinath <70736797+sparsha-2011@users.noreply.github.com>
2025-11-14 20:34:12 -0800

    Update odd-even-linked-list.py

--- LinkedLists/odd-even-linked-list.py
+++ LinkedLists/odd-even-linked-list.py
@@ -34,3 +34,55 @@ class Solution:
         
         odd.next = evenStart  # Append even list after odd list
         return head
+
+#Solution 2
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    
+       
+        cur = head
+        start = head
+        odd_list = ListNode()
+        result = odd_list
+        even_list = ListNode()
+        even_list_start = even_list
+        n = 0
+
+
+        while cur:
+            n+=1
+            cur = cur.next
+        
+        
+
+
+        for i in range(n):
+            if i%2==0:
+                
+                odd_list.next = start
+                odd_list=odd_list.next
+          
+    
+                
+
+ 
+            else:
+                even_list.next = start
+                even_list=even_list.next
+    
+            start=start.next 
+
+        
+        even_list.next = None
+        odd_list.next = even_list_start.next
+
+        
+
+        return result.next
+            
+
